87bbefa8cb78c6dc081dd2aeeecd55e7361bf417
rickyli5962 <103224946+rickyli5962@users.noreply.github.com>
2022-07-31 23:50:40 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -32,13 +32,10 @@ new fullpage('#fullpage', {
 
 // use local object to cache json response
 charts.storage = {};
-// @param callback: a function that works on k, i.e. of the form function(k) {}
 d3.cachedJson = function(url, key, callback) {
 	if (charts.storage[key]) {
-		// data is in the storage
 		callback(JSON.parse(charts.storage[key]));
 	} else {
-		// not cached, fetch the data from url
 		d3.json(url, function(json) {
       charts.storage[key] = JSON.stringify(json);
       callback(json);
