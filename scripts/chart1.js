8040ffdfa2faf8163fbd3db91b3eee8e9ec6d539
rickyli5962 <103224946+rickyli5962@users.noreply.github.com>
2022-08-04 23:30:08 -0400

    Update chart1.js

--- scripts/chart1.js
+++ scripts/chart1.js
@@ -1,19 +1,15 @@
 charts.chart1 = function() {
-  // initialise layout variables
-  const margin = {top: 50, right: 20, bottom: 50, left: 60};
-  const width = 600;
-  const height = 400;
-
+  const margin = {top: 50, right: 50, bottom: 50, left: 50};
+  const width = 800;
+  const height = 600;
   const parseDateTime = d3.timeParse("%B %d, %Y");
 
-  // initialise charts
   const svg = d3.select('#svg1')
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
       .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-  // get data
   const file = 'data/NetflixOriginals.json';
   d3.cachedJson(file, 'chart1', function(data) {
     data.forEach(function(d) {
@@ -34,7 +30,6 @@ charts.chart1 = function() {
   });
 
   function draw(data) {
-    // X axis
     const x = d3.scaleBand()
         .range([0, width])
         .domain(data.map(function (d) {
@@ -48,14 +43,12 @@ charts.chart1 = function() {
         .attr("transform", "translate(-10,0)rotate(-45)")
         .style("text-anchor", "end");
 
-    // Add Y axis
     const y = d3.scaleLinear()
         .domain([0, 200])
         .range([height, 0]);
     svg.append("g")
         .call(d3.axisLeft(y));
 
-    // Bars
     svg.selectAll("mybar")
         .data(data)
         .enter()
@@ -66,7 +59,6 @@ charts.chart1 = function() {
         .attr("height", function(d) { return height - y(d.numOriginals); })
         .attr("fill", "#69b3a2")
 
-    // Features of the annotation
     const annotations = [
       {
         note: {
@@ -96,7 +88,6 @@ charts.chart1 = function() {
       }
     ]
 
-    // Add annotation to the chart
     const makeAnnotations = d3.annotation()
         .annotations(annotations)
     d3.select("#svg1")
