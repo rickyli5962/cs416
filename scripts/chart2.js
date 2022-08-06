65a0e408d4531941ea8d9a2790ac18d0e534d48c
rickyli5962 <103224946+rickyli5962@users.noreply.github.com>
2022-08-05 20:23:06 -0400

    Add files via upload

--- scripts/chart2.js
+++ scripts/chart2.js
@@ -1,9 +1,11 @@
 var finalDataChart2 = [];
 
-const marginChart2 = {top: 50, right: 50, bottom: 50, left: 50};
-const widthChart2 = 500;
-const heightChart2 = 300;
+// initialise layout variables
+const marginChart2 = {top: 50, right: 20, bottom: 50, left: 60};
+const widthChart2 = 600;
+const heightChart2 = 400;
 
+// initialise charts
 const svg = d3.select('#svg2')
     .attr('width', widthChart2 + marginChart2.left + marginChart2.right)
     .attr('height', heightChart2 + marginChart2.top + marginChart2.bottom)
@@ -18,6 +20,7 @@ charts.chart2 = function() {
 function getAndDrawData() {
   const parseDateTime = d3.timeParse("%B %d, %Y");
 
+  // get data
   const file = 'data/NetflixOriginals.json';
   d3.cachedJson(file, 'chart1', function(data) {
     data.forEach(function(d) {
@@ -51,6 +54,7 @@ function drawChart2(data) {
   d3.select('#svg-2-parent-g').selectAll('*').remove();
   svg.selectAll('rect').remove();
 
+  // X axis
   const x = d3.scaleBand()
       .range([0, widthChart2])
       .domain(data.map(function (d) {
@@ -64,12 +68,14 @@ function drawChart2(data) {
       .attr("transform", "translate(-10,0)rotate(-45)")
       .style("text-anchor", "end");
 
+  // Add Y axis
   const y = d3.scaleLinear()
       .domain([0, 7])
       .range([heightChart2, 0]);
   svg.append("g")
       .call(d3.axisLeft(y));
 
+  // Bars
   svg.selectAll("mybar")
       .data(data)
       .enter()
@@ -78,12 +84,26 @@ function drawChart2(data) {
       .attr("y", function(d) { return y(d.averageScore); })
       .attr("width", x.bandwidth())
       .attr("height", function(d) { return heightChart2 - y(d.averageScore); })
-      .attr("fill", "#E50914")
+      .attr("fill", "#69b3a2")
 
+  // Features of the annotation
   const annotations = [
     {
       note: {
-        label: "While the # of Netflix Original are on the rise, the average IMDB score is declining."
+        label: "Decline begins"
+      },
+      connector: {
+        end: "arrow"
+      },
+      type: d3.annotationLabel,
+      x: 250,
+      y: 75,
+      dx: 0,
+      dy: -25
+    },
+    {
+      note: {
+        label: "Worst scores since 2014"
       },
       connector: {
         end: "arrow"
@@ -95,4 +115,82 @@ function drawChart2(data) {
       dy: -25
     }
   ]
+
+  if (params.some(param => !d3.select('#' + param.id).property('checked'))) {
+    // remove annotations
+    d3.select('#svg-2-annotations').selectAll('*').remove();
+    d3.select('#svg-2-annotations').remove();
+  } else {
+    // Add annotation to the chart
+    const makeAnnotations = d3.annotation()
+        .annotations(annotations);
+    d3.select("#svg2")
+        .append("g")
+        .attr('id', 'svg-2-annotations')
+        .call(makeAnnotations);
   }
+}
+
+const params = [
+  {
+    id: "Action",
+  },
+  {
+    id: "Adventure",
+  },
+  {
+    id: "Animation",
+  },
+  {
+    id: "Anime",
+  },
+  {
+    id: "Christmas",
+  },
+  {
+    id: "Comedy",
+  },
+  {
+    id: "Crime",
+  },
+  {
+    id: "Documentary",
+  },
+  {
+    id: "Drama",
+  },
+  {
+    id: "Fantasy",
+  },
+  {
+    id: "Family",
+  },
+  {
+    id: "Musical",
+  },
+  {
+    id: "Mystery",
+  },
+  {
+    id: "Other",
+  },
+  {
+    id: "Romance",
+  },
+  {
+    id: "Science-Fiction",
+  },
+  {
+    id: "Sports-film",
+  },
+  {
+    id: "War",
+  },
+  {
+    id: "Western",
+  },
+];
+
+function updateChart2Data() {
+  getAndDrawData();
+}
