bfe4df99ab15183eb97695c338b0819f413faef9
rickyli5962 <103224946+rickyli5962@users.noreply.github.com>
2022-08-05 20:35:14 -0400

    Update chart2.js

--- scripts/chart2.js
+++ scripts/chart2.js
@@ -1,11 +1,10 @@
 var finalDataChart2 = [];
 
-// initialise layout variables
-const marginChart2 = {top: 50, right: 20, bottom: 50, left: 60};
-const widthChart2 = 600;
-const heightChart2 = 400;
 
-// initialise charts
+const marginChart2 = {top: 50, right: 50, bottom: 50, left: 50};
+const widthChart2 = 500;
+const heightChart2 = 300;
+
 const svg = d3.select('#svg2')
     .attr('width', widthChart2 + marginChart2.left + marginChart2.right)
     .attr('height', heightChart2 + marginChart2.top + marginChart2.bottom)
@@ -20,7 +19,6 @@ charts.chart2 = function() {
 function getAndDrawData() {
   const parseDateTime = d3.timeParse("%B %d, %Y");
 
-  // get data
   const file = 'data/NetflixOriginals.json';
   d3.cachedJson(file, 'chart1', function(data) {
     data.forEach(function(d) {
@@ -54,7 +52,6 @@ function drawChart2(data) {
   d3.select('#svg-2-parent-g').selectAll('*').remove();
   svg.selectAll('rect').remove();
 
-  // X axis
   const x = d3.scaleBand()
       .range([0, widthChart2])
       .domain(data.map(function (d) {
@@ -68,14 +65,12 @@ function drawChart2(data) {
       .attr("transform", "translate(-10,0)rotate(-45)")
       .style("text-anchor", "end");
 
-  // Add Y axis
   const y = d3.scaleLinear()
       .domain([0, 7])
       .range([heightChart2, 0]);
   svg.append("g")
       .call(d3.axisLeft(y));
 
-  // Bars
   svg.selectAll("mybar")
       .data(data)
       .enter()
@@ -86,25 +81,11 @@ function drawChart2(data) {
       .attr("height", function(d) { return heightChart2 - y(d.averageScore); })
       .attr("fill", "#69b3a2")
 
-  // Features of the annotation
   const annotations = [
     {
       note: {
-        label: "Decline begins"
-      },
-      connector: {
-        end: "arrow"
-      },
-      type: d3.annotationLabel,
-      x: 250,
-      y: 75,
-      dx: 0,
-      dy: -25
-    },
-    {
-      note: {
-        label: "Worst scores since 2014"
-      },
+        label: "While Netflix produces more original content year over year, the ratings are declining"
+      }
       connector: {
         end: "arrow"
       },
@@ -131,65 +112,6 @@ function drawChart2(data) {
   }
 }
 
-const params = [
-  {
-    id: "Action",
-  },
-  {
-    id: "Adventure",
-  },
-  {
-    id: "Animation",
-  },
-  {
-    id: "Anime",
-  },
-  {
-    id: "Christmas",
-  },
-  {
-    id: "Comedy",
-  },
-  {
-    id: "Crime",
-  },
-  {
-    id: "Documentary",
-  },
-  {
-    id: "Drama",
-  },
-  {
-    id: "Fantasy",
-  },
-  {
-    id: "Family",
-  },
-  {
-    id: "Musical",
-  },
-  {
-    id: "Mystery",
-  },
-  {
-    id: "Other",
-  },
-  {
-    id: "Romance",
-  },
-  {
-    id: "Science-Fiction",
-  },
-  {
-    id: "Sports-film",
-  },
-  {
-    id: "War",
-  },
-  {
-    id: "Western",
-  },
-];
 
 function updateChart2Data() {
   getAndDrawData();
